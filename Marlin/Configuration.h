163ec0b521b8a88ee3d29f6f828c1afc046fb8c6
Hrafnkell <hrafnkell@gmail.com>
2020-10-08 19:54:21 +0000

    Ender 3 pids

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -493,9 +493,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.0 }
     #define DEFAULT_Kd_LIST { 114.00, 112.0 }
   #else
-    #define DEFAULT_Kp  22.20
-    #define DEFAULT_Ki   1.08
-    #define DEFAULT_Kd 114.00
+    #define DEFAULT_Kp  21.73
+    #define DEFAULT_Ki   1.54
+    #define DEFAULT_Kd 76.55
   #endif
 #endif // PIDTEMP
 
